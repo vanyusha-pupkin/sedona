6f62251556c9c024236d8f7408d2115ec25f906f
vany pupkin <vany_pupkin@mail.ru>
2020-05-22 19:13:07 +0600

    finish header

---
+++ src/js/modules/_main-menu.js
@@ -0,0 +1,18 @@
+
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+//открываем/закрываем выпадающее меню на мобильной версии
+//при нажатии на кнопку "гамбургера"
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+  } else {
+      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+  }
+})
